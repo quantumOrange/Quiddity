0acc091a6536f9cc3251867adeb929b778e5d0e4
David Crooks <david.crooks@wrld3d.com>
2019-01-18 13:27:52 +0100

    refactored pentagram playground, coloured some triangles.

--- Pentagram.playground/Contents.swift
+++ Pentagram.playground/Contents.swift
@@ -2,50 +2,55 @@ import UIKit
 import PlaygroundSupport
 import Quiddity
 
-
-
-let radius = 150.0
-let margin = 5.0
+let radius = 200.0
+let margin = 10.0
 let halfWidth = radius + margin
 
 let drawFrame = CGRect(x:0.0,y:0.0,width:2*halfWidth,height:2*halfWidth)
 
 let circle = Circle(center: Vec2(x:halfWidth, y:halfWidth), radius: radius)
-//_ in Double.random(in: 0 ..< 2*Double.pi
 
-let nAnchors = 5
-let verticies = (1...nAnchors)
-                .map{ Double($0)*2*Double.pi/Double(nAnchors) }
-                .map( circle.evaluate )
+func ngram(verticies:[Vec2]) -> ([Line],[Vec2],[Triangle]) {
+    let lines = zip(verticies, permute(verticies,by: 2))
+        .map(Line.init)
+    
+    let intersections = zip(lines, permute(lines,by: 1))
+        .compactMap(intersect)
+    
+    let triangles = zip(zip(intersections,permute(intersections, by: 1)),permute(verticies, by:2))
+        .map{ Triangle(A: $0.0.0, B: $0.0.1, C: $0.1)}
+    
+    return (lines,intersections,triangles)
+}
 
-let anchors = verticies
-                .map{ Circle.init(center: $0, radius: margin)}
+let nVerticies = 5
 
-let lines = zip(verticies, permute(verticies,by: 2))
-                .map(Line.init)
+let verticies = (1...nVerticies)
+        .map{ Double($0)*2*Double.pi/Double(nVerticies) }
+        .map( circle.evaluate )
 
+let (lines, intersections, triangles) = ngram(verticies:verticies)
 
-let intersectionSpots = zip(lines, permute(lines,by: 1))
-    .compactMap(intersect)
-    .map{Circle(center: $0, radius: margin)}
+let anchors = (verticies + intersections)
+        .map{ Circle.init(center: $0, radius: margin)}
+
 
 ///////////////////////// Draw ////////////////////////
 
 let canvasView = CanvasView(frame: drawFrame)
 canvasView.backgroundColor = UIColor.white
 
-
 var paintables = [Paintable(drawable: circle, fill: nil, stroke: Stroke(color: UIColor.black, weight: 2.0))]
 
-paintables += anchors.map {
-    Paintable(drawable: $0, fill: UIColor.blue, stroke: nil)
+paintables += triangles.map {
+    Paintable(drawable: $0, fill: UIColor.yellow, stroke: nil)
 }
 
 paintables += lines.map {
     Paintable(drawable: $0, fill: nil, stroke: Stroke(color: UIColor.black, weight: 2.0))
 }
 
-paintables += intersectionSpots.map {
+paintables += anchors.map {
     Paintable(drawable: $0, fill: UIColor.blue, stroke: nil)
 }
 
