e7402cefb3cc45e8cd383b12eaa0077ec080fcfc
David Crooks <crodave@gmail.com>
2019-04-29 22:24:46 +0100

    test inteesections

--- QuiddityTests/GeometryPrimitives/CircleTests.swift
+++ QuiddityTests/GeometryPrimitives/CircleTests.swift
@@ -25,6 +25,31 @@ let tolerance = 0.0000001
         XCTAssertEqual(c.area, Double.pi , accuracy:tolerance , "Area of unit circle should be \(Double.pi) but got \(c.area)")
         XCTAssertEqual(c.circumferance, 2.0*Double.pi , accuracy:tolerance , "Circumferance of unit circle should be \(2.0*Double.pi) but got \(c.circumferance)")
     }
+    
+    func testIntersection(){
+        
+        let tolerance = 0.000000001
+        let c1 = Circle(center: Vec2.zero, radius: 1)
+        let c2 = Circle(center: Vec2(x: 1,y: 1), radius: 1)
+        
+        let intersection = c1.intersect(with: c2).sorted(by: {$0.x > $1.x})
+        
+        let expected = [Vec2(x: 1, y: 0),Vec2(x: 0, y: 1)].sorted(by: {$0.x > $1.x})
+        
+        XCTAssert(intersection.count == 2)
+        
+        if let result = intersection.first,
+            let exp = expected.first,
+            let result2 = intersection.last,
+            let exp2 = expected.last
+        {
+            XCTAssertEqual(result.x, exp.x, accuracy: tolerance)
+            XCTAssertEqual(result.y, exp.y, accuracy: tolerance)
+            XCTAssertEqual(result2.x, exp2.x, accuracy: tolerance)
+            XCTAssertEqual(result2.y, exp2.y, accuracy: tolerance)
+        }
+        
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
